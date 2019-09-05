a1616d6db8fb13a3ba1c2d910d0e411c4734e3e6
jose <newmoon798@gmail.com>
2019-09-04 23:10:39 -0500

    agrego dos clases cliente y reserva se mapeo con jpa ademas se agrego de pendencias al pom.xml y por ultimo se agrego la libreria lombok que ayuda para los get y set de todas las clases se agrego un pluyin en el ide lombok

---
+++ src/main/java/com/platzi/ereservation/modelo/Cliente.java
@@ -0,0 +1,34 @@
+package com.platzi.ereservation.modelo;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Clase que representa a la tabla cliente
+ * @author Jose
+ */
+@Data
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid2")
+    private String idCli;
+
+    private String nombreCli;
+    private String apellidoCli;
+    private String identificationCli;
+    private String direcionCli;
+    private String telefonoCli;
+    private String emailCli;
+
+    @OneToMany(mappedBy = "cliente")
+    private Set<Reserva> reservas;
+
+    public Cliente() {}
+}
